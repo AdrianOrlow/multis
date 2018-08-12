ff6db2f7d3d29b41b62e24754f5a6dfeb02d6292
Adrian Orłów <aorlow@mail.com>
2018-08-12 02:46:31 +0200

    💥 Update orlow.me v3.0
    Huuge update of my personal site orlow.me.
    
    Change almost everything.

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,36 @@
+var scroll = new SmoothScroll('a[href*="#"]');
+new WOW().init();
+
+function initMap() {
+    var city = {
+        lat: 50.204987,
+        lng: 19.273931
+    };
+    var map = new google.maps.Map(
+        document.getElementsByClassName('contact--map')[0], {
+            zoom: 11,
+            center: city,
+            styles: [{
+                "featureType": "all",
+                "stylers": [{
+                    "saturation": -100
+                }]
+            }]
+        });
+    var marker = new google.maps.Marker({
+        position: city,
+        map: map
+    });
+}
+
 function toggleNav() {
-    var hC = document.querySelector(".nav--hamburger__content");
-    var hB = document.querySelector(".nav--hamburger a");
-    if (hC.classList.contains("hidden")) {
-        hC.classList.remove("hidden");
-        hB.classList.remove("fa-bars");
-        hB.classList.add("fa-times");
+    var hC = document.querySelector(".nav--hamburger__content").classList;
+    var hB = document.querySelector(".hamburger--btn").classList;
+    if (hC.contains("hidden")) {
+        hC.remove("hidden");
+        hB.add("open");
     } else {
-        hC.classList.add("hidden");
-        hB.classList.remove("fa-times");
-        hB.classList.add("fa-bars");
+        hC.add("hidden");
+        hB.remove("open");
     }
 }
